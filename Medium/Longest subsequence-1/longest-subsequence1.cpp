78fc87f06529e88cb58786e85687769acbf93600
azeemsln <153131338+azeemsln@users.noreply.github.com>
2024-05-27 23:22:27 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest subsequence-1/longest-subsequence1.cpp
+++ Medium/Longest subsequence-1/longest-subsequence1.cpp
@@ -27,16 +27,23 @@ class Solution {
   public:
     int longestSubseq(int n, vector<int> &a) {
         // code here
-         unordered_map<int, int> dp;
-        int x = 0;
-        for (int i = 0; i < n; ++i) {
-            int b = a[i];
-            dp[b] = max(dp[b], dp[b - 1] + 1); 
-            dp[b] = max(dp[b], dp[b + 1] + 1); 
-            x = max(x, dp[b]);
+        map<int, int> dp;
+        int ans = 1;
+
+        for (size_t i = 0; i < n; i++)
+        {
+            dp[a[i]] = max(dp[a[i]], 1);
+            
+            if (dp.count(a[i] - 1))
+                dp[a[i]] = max(dp[a[i]], dp[a[i] - 1] + 1);
+
+            if (dp.count(a[i] + 1))
+                dp[a[i]] = max(dp[a[i]], dp[a[i] + 1] + 1);
+
+            ans = max(ans, dp[a[i]]);
         }
 
-        return x;
+        return ans;
     }
 };
 
