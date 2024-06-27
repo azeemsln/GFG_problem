f8506f88848f0dcaa573b70fa05fd2f5b9efbc28
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-06-27 10:54:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Toeplitz matrix/toeplitz-matrix.java
+++ Difficulty: Medium/Toeplitz matrix/toeplitz-matrix.java
@@ -32,14 +32,9 @@ class Solution {
     /*You are required to complete this method*/
     boolean isToepliz(int mat[][]) {
         // Your code here
-         int n=mat.length;
-        int m=mat[0].length;
-        for(int i=1;i<n;i++)
-        {
-            for(int j=i;j<m;j++)
-            {
-                if(mat[i][j]!=mat[i-1][j-1])
-                return false;
+        for(int i=1;i<mat.length;i++){
+            for(int j=1;j<mat[i].length;j++){
+                if(mat[i][j]!=mat[i-1][j-1]) return false;
             }
         }
         return true;
