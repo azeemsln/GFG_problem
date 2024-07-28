f974f293e737e22198e3b9349cbf637a76fb82b3
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-07-28 11:41:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+
+            Solution ob = new Solution();
+            String result = ob.removeDups(s);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+    class Solution {
+        String removeDups(String str) {
+            // code here
+             StringBuilder sb=new StringBuilder();
+        
+        for(char c:str.toCharArray())
+        {
+            if(sb.indexOf(Character.toString(c))==-1)
+            {
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+        }
+    }
