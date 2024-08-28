fd13b9593c81dace72dc287e42957076fbcc53bc
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-08-28 16:57:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    // Function to sort the array according to frequency of elements.
+    public ArrayList<Integer> sortByFreq(int arr[]) {
+        // add your code here
+       ArrayList<Integer> list=new ArrayList<>();
+        HashMap<Integer,Integer> freq=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
+        }
+        
+        for(int num:arr)
+        {
+            list.add(num);
+        }
+        
+        Collections.sort(list,(a,b)->{
+            int cmp=freq.get(b).compareTo(freq.get(a));
+            if(cmp==0)
+            {
+                return a.compareTo(b);
+            }
+            else{
+                return cmp;
+            }
+        });
+        
+        return list;
+    }
+}
+
+//{ Driver Code Starts.
+
+class Driverclass {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(sc.readLine());
+        while (n != 0) {
+            String input = sc.readLine();
+            String[] inputs = input.split(" ");
+            int[] arr = new int[inputs.length];
+
+            for (int i = 0; i < inputs.length; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
+            }
+
+            ArrayList<Integer> ans = new ArrayList<Integer>();
+            ans = new Solution().sortByFreq(arr);
+            for (int i = 0; i < ans.size(); i++) System.out.print(ans.get(i) + " ");
+            System.out.println();
+            n--;
+        }
+    }
+}
+
+// } Driver Code Ends
