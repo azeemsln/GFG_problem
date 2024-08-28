27f7a02a32bf0eb5405cdc6096091c7069d680c4
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-08-28 17:57:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.java
@@ -14,30 +14,23 @@ class Solution {
     // Function to sort the array according to frequency of elements.
     public ArrayList<Integer> sortByFreq(int arr[]) {
         // add your code here
-         ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        for(int i=0;i<arr.length;i++)
-        {
-            freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
-        }
-        
-        for(int num:arr)
-        {
-            list.add(num);
-        }
-        
-        Collections.sort(list,(a,b)->{
-            int cmp=freq.get(b).compareTo(freq.get(a));
-            if(cmp==0)
-            {
-                return a.compareTo(b);
-            }
-            else{
+         ArrayList<Integer> list=new ArrayList();
+         HashMap<Integer,Integer> map=new HashMap();
+         for(int n:arr){
+             map.put(n,map.getOrDefault(n,0)+1);
+         }
+         for(int n:arr){
+             list.add(n);
+         }
+         Collections.sort(list,(a,b)->{
+             int cmp=map.get(b).compareTo(map.get(a));
+             if(cmp==0){
+                 return a.compareTo(b);
+             }
+             else
                 return cmp;
-            }
-        });
-        
-        return list;
+         });
+         return list;
     }
 }
 
