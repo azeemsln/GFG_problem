c0961b226325a0de0a9917ea4232b0e91bde4893
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-09-13 23:21:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Mirror Tree/mirror-tree.java
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -130,10 +130,10 @@ class Solution {
     void mirror(Node node) {
         // Your code here
         if(node==null) return;
-        Node temp=node.left;
-        node.left=node.right;
-        node.right=temp;        
-        mirror(node.left);
-        mirror(node.right);
+Node temp=node.left;
+node.left=node.right;
+node.right=temp;        
+mirror(node.left);
+mirror(node.right);
     }
 }
