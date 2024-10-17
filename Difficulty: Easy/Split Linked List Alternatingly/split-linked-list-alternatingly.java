068d97d5520a5a1ce4f09069cbda122f70db70a4
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2024-10-17 10:11:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            Scanner ss = new Scanner(input);
+
+            while (ss.hasNextInt()) {
+                arr.add(ss.nextInt());
+            }
+
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+
+            Solution ob = new Solution();
+            Node[] result = ob.alternatingSplitList(head);
+            printList(result[0]);
+            printList(result[1]);
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+    // Function to append a new node with newData at the end of a linked list
+    Node[] alternatingSplitList(Node head) {
+        // code here
+        Node[] res=new Node[2];
+        res[0]=head;
+        res[1]=head.next;
+        Node temp1=res[0];
+        Node temp2=res[1];
+        while(temp1!=null && temp2!=null && temp2.next!=null){
+            temp1.next=temp2.next;
+            temp1=temp1.next;
+            temp2.next=temp1.next;
+            temp2=temp2.next;
+        }
+        temp1.next=null;
+        return res;
+    }
+}
