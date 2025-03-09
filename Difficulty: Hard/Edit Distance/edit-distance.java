61af65ee77aed5bf3c7c8cb41050d8c1c3e2875f
Mohd Azeem <153131338+azeemsln@users.noreply.github.com>
2025-03-10 01:56:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -23,37 +24,20 @@ class GFG {
 
 
 class Solution {
-    public int editDistance(String str1, String str2) {
+    public int editDistance(String s1, String s2) {
         // Code here
-         int m = str1.length();
-        int n = str2.length();
-        
-        // Create a DP table to store the results of subproblems
-        int[][] dp = new int[m + 1][n + 1];
-        
-        // Fill dp[][] in bottom-up manner
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                // If first string is empty, the only option is to insert all characters of second string
-                if (i == 0) {
-                    dp[i][j] = j; // Insert all j characters
-                }
-                // If second string is empty, the only option is to remove all characters of the first string
-                else if (j == 0) {
-                    dp[i][j] = i; // Remove all i characters
-                }
-                // If last characters are the same, ignore the last character and get the count for the remaining strings
-                else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-                // If the last character is different, consider all possibilities and find the minimum
-                else {
-                    dp[i][j] = 1 + Math.min(dp[i][j - 1], // Insert
-                                    Math.min(dp[i - 1][j], // Remove
-                                             dp[i - 1][j - 1])); // Replace
-                }
-            }
-        }
-        return dp[m][n];
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n][m];
+        for(int[] d: dp) Arrays.fill(d, -1);
+        return func(n-1, m-1, s1, s2, dp);
+    }
+    
+    int func(int i, int j, String s1, String s2, int[][] dp) {
+        if(i < 0) return j + 1;
+        if(j < 0) return i+1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j)) return func(i-1, j-1, s1, s2, dp);
+        return dp[i][j] = 1 + Math.min(func(i, j-1, s1, s2, dp), Math.min(func(i-1, j, s1, s2, dp), func(i-1, j-1, s1, s2, dp)));
     }
 }
